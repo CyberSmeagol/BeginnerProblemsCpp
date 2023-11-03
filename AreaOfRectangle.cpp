73e0631f7bacf3640631ffd7ef5c3e3be1de1a40
CyberSmeagol <148290139+CyberSmeagol@users.noreply.github.com>
2023-11-03 23:56:44 +0500

    Create AreaOfRectangle.cpp

---
+++ AreaOfRectangle.cpp
@@ -0,0 +1,15 @@
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int length;
+    int width;
+    cout<<"enter length: ";
+   cin>>length; 
+    cout<<"enter width: ";
+    cin>>width;
+    cout<<"area of rectangle is "<<length*width<<endl;
+    return 0;
+}
